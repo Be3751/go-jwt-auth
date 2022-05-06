cb556f28376760bcb5593b4fbceea4ed2f8357c2
薗部匡矢 <msonobe@MacBook-Pro.local>
2022-05-06 20:05:19 +0900

    add func to read key-string

--- crypto/auth.go
+++ crypto/auth.go
@@ -1,6 +1,8 @@
 package crypto
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 	"time"
 
@@ -8,8 +10,8 @@ import (
 )
 
 var (
-	secret   = os.Getenv("SECRET_KEY") // secret key to encrypt header.payload
-	lifetime = 30 * time.Minute        // token's lifetime is set to 30 minutes
+	secret   = ""               // secret key to encrypt header.payload
+	lifetime = 30 * time.Minute // token's lifetime is set to 30 minutes
 )
 
 // Auth は署名前の認証トークン情報を表す。
@@ -20,12 +22,20 @@ type Auth struct {
 
 // JWTの生成
 func GenerateToken(userID string, now time.Time) (string, error) {
+	secret, err := readSecretKey(".secret.key")
+	if err != nil {
+		fmt.Println("Couldn't read secret key.: ", err)
+		return "", err
+	}
+
 	// 署名なしトークンの生成：header.payload encorded by Base64
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user_id":    userID,
 		"issued_at":  now.Unix(),
 		"expired_at": now.Add(lifetime).Unix(),
 	})
+
+	fmt.Println("secret key is", secret)
 
 	// 署名付きトークンの生成：header.payload.signature encorded by Base64
 	// header.payload -> signature encrypted by HS256 with a secret key
@@ -34,6 +44,12 @@ func GenerateToken(userID string, now time.Time) (string, error) {
 
 // JWTの検証と返却
 func VerifyToken(signedToken string) (*jwt.Token, error) {
+	secret, err := readSecretKey(".secret.key")
+	if err != nil {
+		fmt.Println("Couldn't read secret key.: ", err)
+		return nil, err
+	}
+
 	// 秘密鍵を用いて暗号化したHeader+Payload部とSignature部を比較することで，トークンの整合性を確認
 	token, err := jwt.Parse(signedToken, func(parsedToken *jwt.Token) (interface{}, error) {
 		return []byte(secret), nil
@@ -42,4 +58,30 @@ func VerifyToken(signedToken string) (*jwt.Token, error) {
 		return nil, err
 	}
 	return token, nil
+}
+
+// keyファイルから秘密鍵を取得
+func readSecretKey(filePath string) (string, error) {
+	keyStr := ""
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	// ファイルを1行ごとに読み込み
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "-----BEGIN SECRET KEY-----" || text == "-----END SECRET KEY-----" {
+			continue
+		}
+		keyStr = keyStr + scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	return keyStr, nil
 }
